8246890d2d8290da0c2a3ac09b12daf87644668f
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-04-16 10:13:17 +0300

    add missing doctypes

---
+++ common/helpdesk/facility_maintenance_support/facility_maintenance_support.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2024, Mawred and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+from cipher.utils import EmployeeFetch
+
+
+class FacilityMaintenanceSupport(EmployeeFetch):
+    
+    def befor_save(self):
+
+        super().before_insert()
---
+++ common/helpdesk/server_request/server_request.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2024, Mawred and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+from cipher.utils import EmployeeFetch
+
+
+class ServerRequest(EmployeeFetch):
+
+    def befor_save(self):
+
+        super().before_insert()
+    
