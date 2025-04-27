8faa650755dc1e96f1838e3d67ce1181a80465a8
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-04-27 12:21:39 +0300

    add validations and logics

--- common/common_customization/doctype/cancel_leave_application/cancel_leave_application.js
+++ common/common_customization/doctype/cancel_leave_application/cancel_leave_application.js
@@ -1,8 +1,28 @@
 // Copyright (c) 2025, Ahmed Madi and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Cancel Leave Application", {
-// 	refresh(frm) {
+frappe.ui.form.on("Cancel Leave Application", {
+    refresh(frm) {
+        frm.set_query("employee", (doc) => {
+            return {
+                filters: {
+                    status: "Active"
+                }
+            }
+        });
 
-// 	},
-// });
+        frm.set_query("leave_application", (doc) => {
+            return {
+                filters: {
+                    status: "Approved",
+                    docstatus: 1,
+                    employee: doc.employee,
+                }
+            }
+        });
+    },
+    employee(frm) {
+        frm.set_value("leave_application", null);
+        frm.refresh_field("leave_application");
+    }
+});
--- common/common_customization/doctype/cancel_leave_application/cancel_leave_application.py
+++ common/common_customization/doctype/cancel_leave_application/cancel_leave_application.py
@@ -1,9 +1,29 @@
 # Copyright (c) 2025, Ahmed Madi and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _, bold
+from frappe.utils import nowdate, date_diff
 from frappe.model.document import Document
 
 
 class CancelLeaveApplication(Document):
-	pass
+	def validate(self):
+		self.validate_employee()
+		self.validate_leave_application()
+
+	def validate_employee(self):
+		employee_status = frappe.db.get_value("Employee", self.employee, "status")
+		if employee_status != "Active":
+			frappe.throw(_("Leave application cancellation is restricted to active employees"))
+	
+	def validate_leave_application(self):
+		leave = frappe.get_doc("Leave Application", self.leave_application)
+		if leave.employee != self.employee:
+			frappe.throw(_("Leave Application {0} is not Belong to Employee {1}").format(bold(self.leave_application), bold(self.employee)))
+		
+		if leave.docstatus != 1 or leave.status != "Approved":
+			frappe.throw(_("Only approved applications can be canceled"))
+		
+		if date_diff(leave.from_date, nowdate()) < 1:
+			frappe.throw(_("Unable to cancel application currently in progress"))
