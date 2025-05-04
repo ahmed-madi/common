83e3e9ea2a11776aa6905b744c6fb7a7a92a0a5e
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-05-04 23:36:09 +0300

    handle cancel leave application

--- common/common_customization/doctype/cancel_leave_application/cancel_leave_application.py
+++ common/common_customization/doctype/cancel_leave_application/cancel_leave_application.py
@@ -3,27 +3,40 @@
 
 import frappe
 from frappe import _, bold
-from frappe.utils import nowdate, date_diff
+from frappe.utils import nowdate, date_diff, get_link_to_form, cint
 from frappe.model.document import Document
 
+from hrms.hr.utils import validate_active_employee
 
 class CancelLeaveApplication(Document):
 	def validate(self):
-		self.validate_employee()
+		validate_active_employee(self.employee)
 		self.validate_leave_application()
+		self.validate_previous_records()
 
-	def validate_employee(self):
-		employee_status = frappe.db.get_value("Employee", self.employee, "status")
-		if employee_status != "Active":
-			frappe.throw(_("Leave application cancellation is restricted to active employees"))
-	
 	def validate_leave_application(self):
 		leave = frappe.get_doc("Leave Application", self.leave_application)
 		if leave.employee != self.employee:
 			frappe.throw(_("Leave Application {0} is not Belong to Employee {1}").format(bold(self.leave_application), bold(self.employee)))
 		
 		if leave.docstatus != 1 or leave.status != "Approved":
-			frappe.throw(_("Only approved applications can be canceled"))
+			frappe.throw(_("Only approved applications can be canceled"), frappe.InvalidStatusError)
 		
 		if date_diff(leave.from_date, nowdate()) < 1:
-			frappe.throw(_("Unable to cancel application currently in progress"))
+			frappe.throw(_("Unable to cancel application currently in progress"), frappe.InvalidStatusError)
+		cancel_allowed_before = cint(frappe.db.get_single_value("Company Policy", "cancel_allowed_before"))
+		if cancel_allowed_before > 0 and date_diff(leave.from_date, self.request_date) < cancel_allowed_before:
+			frappe.throw(_("Unable to cancel application currently in progress"), frappe.InvalidStatusError)
+	
+	def validate_previous_records(self):
+		prev =  frappe.get_all(
+			"Cancel Leave Application",
+			filters={"leave_application": self.leave_application, "employee": self.employee, "docstatus": ["!=", 2], "name": ["!=", self.name]}
+		)
+		if len(prev) > 0:
+			frappe.throw(_("Employee has leave cancellation detected {0}").format(get_link_to_form("Cancel Leave Application", prev[0].name)), frappe.UniqueValidationError)
+	
+	def on_submit(self):
+		leave = frappe.get_doc("Leave Application", self.leave_application)
+		leave.cancel()
+		leave.add_comment(text="Cancelled by employee {}".format(self.name))
