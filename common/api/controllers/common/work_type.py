dd8a274561759e20da4f51e59c6806afe28e60a0
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-05-07 15:55:03 +0300

    add routes and controllers for common leave doctypes

---
+++ common/api/controllers/common/work_type.py
@@ -0,0 +1,12 @@
+from common.api.utils.endpoints import document_list, read_doc
+
+def work_type_list():
+    doctype = "Work Type"
+    fields = ["name", "type"]
+    return document_list(doctype, fields)
+
+def read_work_type(name: str):
+    doctype = "Work Type"
+    fields = ["name", "type"]
+    return read_doc(doctype, name, origin_fields=fields)
+
--- common/api/routes/__init__.py
+++ common/api/routes/__init__.py
@@ -1,7 +1,21 @@
+# Common Data
+from common.api.routes.common.employee import employee_rules
+from common.api.routes.common.holiday_list import holiday_list_rules
+from common.api.routes.common.leave_type import leave_type_rules
+from common.api.routes.common.salary_component import salary_component_rules
+from common.api.routes.common.work_type import work_type_rules
+
 # User Auth/Info
 from common.api.routes.user import user_rules
 
 url_rules = []
+
+# Common Data
+url_rules += employee_rules
+url_rules += holiday_list_rules
+url_rules += leave_type_rules
+url_rules += salary_component_rules
+url_rules += work_type_rules
 
 # User Auth/Info
 url_rules += user_rules
