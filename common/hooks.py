94dcab5989c1398ea56a03f6afa1a2f9a12c57d4
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-05-07 15:44:03 +0300

    handle new status changes

--- common/hooks.py
+++ common/hooks.py
@@ -46,7 +46,9 @@ app_include_css = [
 
 # include js in doctype views
 # doctype_js = {"doctype" : "public/js/doctype.js"}
-# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
+doctype_list_js = {
+    "Attendance" : "public/js/attendance_list.js"
+}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
 
