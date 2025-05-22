fa6592ddcc7ddd3fb7a34829588f4bd48798ad60
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-05-22 07:37:52 +0300

    delete helpdesk module

--- common/helpdesk/doctype/facility_maintenance_support/facility_maintenance_support.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Mawred and contributors
-# For license information, please see license.txt
-
-# import frappe
-from common.utils.hr import EmployeeFetch
-
-class FacilityMaintenanceSupport(EmployeeFetch):
-    def before_validate(self):
-        super().before_insert()
