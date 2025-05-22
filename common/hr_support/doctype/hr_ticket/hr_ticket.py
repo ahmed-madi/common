7ddd715537025f0abe70d927dc056a2af190041b
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-05-22 17:00:45 +0300

    help desk tickets

---
+++ common/hr_support/doctype/hr_ticket/hr_ticket.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2025, Ahmed Madi and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+from frappe.model.document import Document
+
+
+class HRTicket(Document):
+    def validate(self):
+        if self.has_value_changed("rating") and self.docstatus != 1 and self.status != 'Closed':
+            frappe.throw(_("Rating allowed for closed tickets only"))
+    
