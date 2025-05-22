020df582b22468a85c3d85872898135ab41c3892
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-05-22 17:01:04 +0300

    visa application

--- common/common_customization/doctype/visa_application/visa_application.py
+++ common/common_customization/doctype/visa_application/visa_application.py
@@ -2,8 +2,10 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
+from frappe.utils import nowdate
+from common.models.base_hr_document import BaseHRDocument
 
-
-class VisaApplication(Document):
-	pass
+class VisaApplication(BaseHRDocument):
+    def before_validate(self):
+        if not self.request_date:
+            self.request_date = nowdate()
