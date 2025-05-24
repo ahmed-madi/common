4b368a3aa7aa806984b2559d040a5d002dec9ee6
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-05-24 17:25:56 +0300

    clearance letter request endpoinnts

--- common/api/routes/__init__.py
+++ common/api/routes/__init__.py
@@ -1,4 +1,5 @@
 # Common Data
+from common.api.routes.common.clearance_purpose import clearance_purpose_rules
 from common.api.routes.common.club import club_rules
 from common.api.routes.common.company_policy import company_policy_rules
 from common.api.routes.common.employee import employee_rules
@@ -20,6 +21,7 @@ from common.api.routes.helpdesk.ticket import ticket_rules
 from common.api.routes.hr_requests.access_system import access_system_rules
 from common.api.routes.hr_requests.education_allowance import education_allowance_rules
 from common.api.routes.hr_requests.change_iban import change_iban_rules
+from common.api.routes.hr_requests.clearance_letter import clearance_letter_rules
 from common.api.routes.hr_requests.club_request import club_request_rules
 from common.api.routes.hr_requests.document_request import document_request_rules
 from common.api.routes.hr_requests.salary_fixation import salary_fixation_rules
@@ -42,6 +44,7 @@ from common.api.routes.user import user_rules
 url_rules = []
 
 # Common Data
+url_rules += clearance_purpose_rules
 url_rules += club_rules
 url_rules += company_policy_rules
 url_rules += employee_rules
@@ -63,6 +66,7 @@ url_rules += ticket_rules
 url_rules += access_system_rules
 url_rules += education_allowance_rules
 url_rules += change_iban_rules
+url_rules += clearance_letter_rules
 url_rules += club_request_rules
 url_rules += document_request_rules
 url_rules += salary_fixation_rules
