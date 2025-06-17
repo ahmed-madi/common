c456b9a20cd4e227dbc605d89f9a18c949d6c5d1
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-06-17 15:39:54 +0300

    fix check message for new record

--- common/hr_support/doctype/hr_ticket/hr_ticket.py
+++ common/hr_support/doctype/hr_ticket/hr_ticket.py
@@ -3,11 +3,14 @@
 
 import frappe
 from frappe import _
+from frappe.utils import flt
 from frappe.model.document import Document
 
 
 class HRTicket(Document):
     def validate(self):
+        if self.is_new() and flt(self.rating) == 0:
+            return
         if self.has_value_changed("rating") and self.docstatus != 1 and self.status != 'Closed':
             frappe.throw(_("Rating allowed for closed tickets only"))
     
