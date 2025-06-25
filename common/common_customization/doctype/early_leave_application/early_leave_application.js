084f1887788fbb74221e0e0f2a888e1a7c9ae821
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-06-25 15:51:41 +0300

    fix and enhance early leave request

--- common/common_customization/doctype/early_leave_application/early_leave_application.js
+++ common/common_customization/doctype/early_leave_application/early_leave_application.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2025, Ahmed Madi and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Early Leave Application", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Early Leave Application", {
+    refresh(frm) {
+        frm.set_query("employee", (doc) => {
+            return {
+                filters: {
+                    status: "Active",
+                },
+            };
+        });
+    },
+});
--- common/common_customization/doctype/early_leave_application/early_leave_application.py
+++ common/common_customization/doctype/early_leave_application/early_leave_application.py
@@ -4,13 +4,12 @@
 import frappe
 from frappe import _
 from frappe.utils import cint, flt, time_diff_in_hours, time_diff_in_seconds, get_first_day, get_last_day, getdate
-from frappe.model.document import Document
 from hrms.hr.doctype.shift_assignment.shift_assignment import get_employee_shift, get_shift_details
-from hrms.hr.doctype.employee_checkin.employee_checkin import add_log_based_on_employee_field
 from common.utils import get_combine_datetime
-
-class EarlyLeaveApplication(Document):
+from common.models.base_hr_document import BaseHRDocument
+class EarlyLeaveApplication(BaseHRDocument):
     def validate(self):
+        super().validate()
         self.validate_on_record_in_same_day()
         self.validate_exit_date_time()
     
@@ -26,10 +25,16 @@ class EarlyLeaveApplication(Document):
 
     def validate_exit_date_time(self):
         policy = frappe.get_doc("Company Policy")
-        if cint(policy.validate_early_exit) == 0:
-            return
+
         exit_datetime = get_combine_datetime(self.exit_date, self.exit_time)
         employee_shift = get_employee_shift(self.employee, exit_datetime, True, 'forward')
+        # If the employee is not assigned to a shift.
+        if cint(policy.do_not_permit_early_leave) == 1:
+            if not employee_shift or employee_shift is None:
+                frappe.throw(_("Application for early leave is not allowed without an assigned shift"))
+
+        if cint(policy.validate_early_exit) == 0:
+            return
         
         if not employee_shift:
             employee_shift = policy.early_exit_shift_type
@@ -65,14 +70,11 @@ class EarlyLeaveApplication(Document):
             frappe.throw(_("You have exceeded the allowed exit hours for this month. {} hours Allowed, {} requested").format(flt(policy.early_exit_total_hours_allowed, 2), flt(total_in_month, 2)))
 
     def on_submit(self):
-        if self.status in ["Open", "Cancelled"]:
-            frappe.throw(_("Only Applications with status 'Approved' and 'Rejected' can be submitted"))
+        if self.status != "Approved":
+            return
         self.mark_attendance_log_in_exit_date()
     
     def mark_attendance_log_in_exit_date(self):
-        if self.status != "Approved":
-            return
-
         exit_datetime = get_combine_datetime(self.exit_date, self.exit_time)
         employee_shift = get_employee_shift(self.employee, exit_datetime, True, 'forward')
         shift_type = employee_shift.get("shift_type", {})
@@ -85,6 +87,3 @@ class EarlyLeaveApplication(Document):
         doc.time = shift_end_time
         doc.log_type = 'OUT'
         doc.insert()
-
-    def before_cancel(self):
-        self.status = "Cancelled"
