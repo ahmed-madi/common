d76329330e1f882c2c0de816b1c8feef7c02a557
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-06-25 16:30:33 +0300

    refactor and enhance request validations

---
+++ common/common_customization/doctype/document_request/document_request_list.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2025, Ahmed Madi and contributors
+// For license information, please see license.txt
+
+frappe.listview_settings["Document Request"] = {
+	add_fields: [
+		"employee",
+		"employee_name",
+		"from_date",
+		"to_date",
+	],
+	has_indicator_for_draft: 1,
+	get_indicator: function (doc) {
+		const status_color = {
+			Approved: "green",
+			Rejected: "red",
+			Open: "orange",
+			Draft: "red",
+			Cancelled: "red",
+			Submitted: "blue",
+		};
+		const status =
+			!doc.docstatus && ["Approved", "Rejected"].includes(doc.status) ? "Draft" : doc.status;
+		return [__(status), status_color[status], "status,=," + doc.status];
+	},
+};
