086a0a7a9ee9e79905690b2c2ad9411ee9dc7767
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-06-25 14:09:41 +0300

    add missing by condition field to missing list

--- common/overrides/leave_application.py
+++ common/overrides/leave_application.py
@@ -2,11 +2,25 @@ import frappe
 from frappe import _
 from hrms.hr.doctype.leave_application.leave_application import LeaveApplication as BaseLeaveApplication
 
-class LeaveApplication(BaseLeaveApplication):
+from common.models.base_hr_document import BaseHRDocument
+
+class LeaveApplication(BaseLeaveApplication, BaseHRDocument):
     def before_validate(self):
         if hasattr(super(), 'before_validate'):
             super().before_validate()
         self.make_reason_clean()
+    
+    def _get_missing_mandatory_fields(self):
+        missing = super()._get_missing_mandatory_fields()
+        if frappe.db.get_value("Leave Type", self.leave_type, "attachment_required") == 1:
+            df = self.meta.get("fields", {"fieldname": ("=", "attachment")})[0]
+            if self.get(df.fieldname) in (None, []) or not self.has_content(df):
+                missing.append((df.fieldname, self.get_msg(df)))
+        if frappe.db.get_value("Leave Type", self.leave_type, "reason_required") == 1:
+            df = self.meta.get("fields", {"fieldname": ("=", "description")})[0]
+            if self.get(df.fieldname) in (None, []) or not self.has_content(df):
+                missing.append((df.fieldname, self.get_msg(df)))
+        return missing
     
     def make_reason_clean(self):
         if not isinstance(self.description, str):
