861d735a49c5229705a840eb7d2b6fccf9ba3026
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-06-26 09:12:35 +0300

    refactor and enhance request validations

--- common/common_customization/doctype/education_allowance_request/education_allowance_request.py
+++ common/common_customization/doctype/education_allowance_request/education_allowance_request.py
@@ -3,7 +3,7 @@
 
 import frappe
 from frappe import _
-from frappe.utils import nowdate, flt, get_link_to_form
+from frappe.utils import nowdate, flt, get_link_to_form, get_year_start, get_year_ending, getdate
 
 from erpnext import get_default_cost_center
 
@@ -34,11 +34,24 @@ class EducationAllowanceRequest(BaseHRDocument):
     def validate_requested_amount(self):
         if flt(self.amount_requested) <= 0:
             frappe.throw(_("Requested amount can not be zero or negative number"))
+        education_allowance_whitelist = flt(frappe.db.get_single_value("Company Policy", "education_allowance_whitelist"))
+        if education_allowance_whitelist in frappe.get_roles(frappe.session.user):
+            return
+        
         max_education_allowance = flt(frappe.db.get_single_value("Company Policy", "max_education_allowance"))
-        if max_education_allowance > 0 and flt(self.amount_requested) > max_education_allowance:
+        sum = frappe.db.sql("""
+                            SELECT SUM(amount_requested)
+                            FROM `tabEducation Allowance Request`
+                            WHERE name!='{}' AND academic_year='{}' AND employee='{}'
+                                AND docstatus=1 AND status='Approved'
+                        """.format(self.name, self.academic_year, self.employee))
+        if not sum:
+            return
+        sum = flt(sum[0][0])
+        if max_education_allowance > 0 and sum > max_education_allowance:
             frappe.throw(
-                _("You have exceeded the maximum allowed education allowance, {}").format(
-                    max_education_allowance,
+                _("You have exceeded the maximum allowed education allowance for year {}, maximum allowed amount {}").format(
+                    self.academic_year, max_education_allowance,
                 )
             )
 
