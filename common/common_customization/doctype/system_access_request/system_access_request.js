6485eb19919e479514bec49288d2d58dff83bbc0
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-06-26 08:41:54 +0300

    refactor and enhance request validations

--- common/common_customization/doctype/system_access_request/system_access_request.js
+++ common/common_customization/doctype/system_access_request/system_access_request.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2025, Ahmed Madi and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("System Access Request", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("System Access Request", {
+	refresh(frm) {
+        frm.set_query("employee", function (doc) {
+            return {
+                filters: {
+                    status: "Active",
+                },
+            };
+        });
+    },
+});
