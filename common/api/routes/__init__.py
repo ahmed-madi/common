7f3952af0ac070b700586dd667c1d5d4e0c86af7
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-07-03 13:03:52 +0300

    expose routes

--- common/api/routes/__init__.py
+++ common/api/routes/__init__.py
@@ -14,9 +14,10 @@ from common.api.routes.common.salary_fixation_reason import salary_fixation_reas
 from common.api.routes.common.system_access_level import system_access_level_rules
 from common.api.routes.common.work_type import work_type_rules
 
-# HelpDesk Requests
+# Help Desk Requests
 from common.api.routes.helpdesk.issue import ticket_issue_rules
-from common.api.routes.helpdesk.request_to_management import request_to_management_rules
+from common.api.routes.helpdesk.employee_feedback import employee_feedback_rules
+from common.api.routes.helpdesk.employee_inquiry import request_to_management_rules
 from common.api.routes.helpdesk.ticket_comment import ticket_comment_rules
 from common.api.routes.helpdesk.ticket import ticket_rules
 
@@ -69,6 +70,7 @@ url_rules += work_type_rules
 
 # HelpDesk Requests
 url_rules += ticket_issue_rules
+url_rules += employee_feedback_rules
 url_rules += request_to_management_rules
 url_rules += ticket_comment_rules
 url_rules += ticket_rules
