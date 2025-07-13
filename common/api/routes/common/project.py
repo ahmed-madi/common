b7c7ec0e81354ecc47b392af4a2d79c7c55df7b0
Abdallah A. Zaqout <26047413+zaqoutabed@users.noreply.github.com>
2025-07-13 19:00:54 +0300

    change var name

--- common/api/routes/common/project.py
+++ common/api/routes/common/project.py
@@ -1,7 +1,7 @@
 from werkzeug.routing import Rule
 from common.api.controllers.common.project import project_list, read_project
 
-club_rules = [
+project_rules = [
 	Rule("/hr-common/project", methods=["GET"], endpoint=project_list),
 	Rule("/hr-common/project/<path:name>/", methods=["GET"], endpoint=read_project),
 ]
