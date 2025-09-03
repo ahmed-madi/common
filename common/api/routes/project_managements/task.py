f1068cb00d6dad12e2f0600d7fe74f18e6c7a72d
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-09-03 12:58:04 +0300

    timesheet details

--- common/api/routes/project_managements/task.py
+++ common/api/routes/project_managements/task.py
@@ -1,12 +1,14 @@
 from werkzeug.routing import Rule
-from common.api.controllers.project_managements.task import task_list, create_task, read_task, update_task, delete_task, add_timesheet, timesheet_list
+from common.api.controllers.project_managements.task import task_list, create_task, read_task, update_task, delete_task, add_timesheet, timesheet_list_for_task, timesheet_list, read_timesheet
 
 task_rules = [
 	Rule("/project-managements/task", methods=["GET"], endpoint=task_list),
 	Rule("/project-managements/task", methods=["POST"], endpoint=create_task),
-	Rule("/project-managements/task/<path:name>/timesheet", methods=["GET"], endpoint=timesheet_list),
+	Rule("/project-managements/task/<path:name>/timesheet", methods=["GET"], endpoint=timesheet_list_for_task),
 	Rule("/project-managements/task/<path:name>/timesheet", methods=["POST"], endpoint=add_timesheet),
 	Rule("/project-managements/task/<path:name>/", methods=["GET"], endpoint=read_task),
 	Rule("/project-managements/task/<path:name>/", methods=["PUT"], endpoint=update_task),
 	Rule("/project-managements/task/<path:name>/", methods=["DELETE"], endpoint=delete_task),
+    Rule("/project-managements/timesheet", methods=["GET"], endpoint=timesheet_list),
+	Rule("/project-managements/timesheet/<path:name>", methods=["GET"], endpoint=read_timesheet),
 ]
