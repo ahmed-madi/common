c10a99cb951fc87022e9a76fd9c2bdafa714af9f
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-09-11 14:40:46 +0300

    user seettings endpoint

--- common/api/controllers/user/firebase.py
+++ common/api/controllers/user/firebase.py
@@ -9,7 +9,7 @@ from common.api.utils.endpoints import (
     update_doc,
     delete_doc,
 )
-from common.api.utils.response import build_success_response, build_error_response
+from common.api.utils.response import build_success_response, build_error_response, handle_exception_response
 
 
 # test workflow
@@ -59,10 +59,88 @@ def unsubscribe():
     build_success_response(status_code=200, message="FCM Token Deleted", data={})
 
 
+@frappe.whitelist(methods=["GET"])
+def get_user_settings():
+    try:
+        user = frappe.get_doc("User", frappe.session.user)
+        if not user.has_permission("read"):
+            raise frappe.PermissionError
+        user.apply_fieldlevel_read_permissions()
+
+        exists = frappe.db.exists("HR Notification Settings", {"user": frappe.session.user})
+        if exists:
+            doc = frappe.get_doc("HR Notification Settings", exists)
+        else:
+            doc = frappe.new_doc("HR Notification Settings")
+            doc.update(
+                {
+                    "user": frappe.session.user,
+                    "task_assignments": 1,
+                    "request_approvals": 1,
+                    "calendar_events": 1,
+                    "helpdesk_updates": 1,
+                    "performance_reviews": 1,
+                }
+            )
+            doc.save(ignore_permissions=True)
+            frappe.db.commit()
+        doc = {
+            "user": doc.user,
+            "task_assignments": doc.task_assignments,
+            "request_approvals": doc.request_approvals,
+            "calendar_events": doc.calendar_events,
+            "helpdesk_updates": doc.helpdesk_updates,
+            "performance_reviews": doc.performance_reviews,
+
+            "language": user.language,
+            "theme": user.desk_theme,
+        }
+
+        return build_success_response(200, f"user settings fetched", doc, {})
+    except Exception as exc:
+        http_status_code = 500
+        message = exc
+        if hasattr(exc, "http_status_code"):
+            http_status_code = exc.http_status_code
+        if hasattr(exc, "args"):
+            args = exc.args
+            if len(args) > 0 and isinstance(args[0], str):
+                message = args[0].split(":")[0]
+            elif len(args) > 1 and isinstance(args[0], int):
+                message = args[1]
+        return build_error_response(
+            http_status_code, f"failed to read user settings", message
+        )
+
 @frappe.whitelist(methods=["POST"])
-def update_notifications_settings():
-    data = get_request_form_data()
-    exists = frappe.db.exists("HR Notification Settings", {"user": frappe.session.user})
+def update_user_settings():
+    try:
+        data = get_request_form_data()
+        doc1 = set_notification_settings(data, frappe.session.user)
+        doc2 = set_user_settings(data, frappe.session.user)
+        doc1.update(doc2)
+        return build_success_response(200, f"user settings updated", doc1)
+    except Exception as exc:
+        http_status_code = 500
+        message = exc
+        if hasattr(exc, "http_status_code"):
+            http_status_code = exc.http_status_code
+        if hasattr(exc, "args"):
+            args = exc.args
+            if len(args) > 0 and isinstance(args[0], str):
+                message = args[0].split(":")[0]
+            elif len(args) > 1 and isinstance(args[0], int):
+                message = args[1]
+        return build_error_response(
+            http_status_code, f"failed to update user settings", message
+        )
+
+
+def set_notification_settings(data, user):
+    if "task_assignments" not in data and "request_approvals" not in data and "calendar_events" not in data and "helpdesk_updates" not in data and "performance_reviews" not in data:
+        return
+
+    exists = frappe.db.exists("HR Notification Settings", {"user": user})
     if exists:
         doc = frappe.get_doc("HR Notification Settings", exists)
     else:
@@ -107,5 +185,33 @@ def update_notifications_settings():
                 "performance_reviews": cint(data.get("performance_reviews")),
             }
         )
-
     doc.save(ignore_permissions=True)
+    return {
+        "user": doc.user,
+        "task_assignments": doc.task_assignments,
+        "request_approvals": doc.request_approvals,
+        "calendar_events": doc.calendar_events,
+        "helpdesk_updates": doc.helpdesk_updates,
+        "performance_reviews": doc.performance_reviews,
+    }
+
+def set_user_settings(data, user):
+    if "language" not in data and "theme" not in data:
+        return
+    exists = frappe.db.exists("User", {"name": user})
+    if not exists:
+        return
+    user = frappe.get_doc("User", exists)
+    if "language" in data:
+        user.update({
+            "language": data.get("language"),
+        })
+    if "theme" in data:
+        user.update({
+            "desk_theme": data.get("theme"),
+        })
+    user.save(ignore_permissions=True)
+    return {
+        "language": user.language,
+        "theme": user.desk_theme,
+    }
