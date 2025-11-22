89c0a80ff6e7aafec5dee1b52307905b59d50999
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-11-22 13:16:28 +0300

    enhance API in club doctype

--- common/api/controllers/common/club.py
+++ common/api/controllers/common/club.py
@@ -1,13 +1,12 @@
 from common.api.utils.endpoints import document_list, read_doc
 
 fields = ["name", "club_name"]
+doctype = "Club"
 
 
 def club_list():
-    doctype = "Club"
-    return document_list(doctype, fields, translate_text=True, tr_field="club_name")
+    return document_list(doctype, fields)
 
 
 def read_club(name: str):
-    doctype = "Club"
-    return read_doc(doctype, name, origin_fields=fields)
+    return read_doc(doctype, name)
