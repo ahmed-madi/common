f5b9436df4220a5f3f7d86114a5c7e8c77442314
zaqouttahir <tahir.a.zaqout@gmail.com>
2026-01-22 16:24:43 +0200

    share task with employee

--- common/hooks.py
+++ common/hooks.py
@@ -150,13 +150,11 @@ override_doctype_class = {
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-# 	}
-# }
+doc_events = {
+    "Task": {
+        "on_update": "common.events.task.on_update",
+    }
+}
 
 # Scheduled Tasks
 # ---------------
