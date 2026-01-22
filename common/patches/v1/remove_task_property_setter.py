6f0530c7548f8d58fae6abd2f0c83efceb9bee99
zaqouttahir <tahir.a.zaqout@gmail.com>
2026-01-22 12:19:42 +0200

    update patch

--- common/patches/v1/remove_task_property_setter.py
+++ common/patches/v1/remove_task_property_setter.py
@@ -4,9 +4,13 @@ import frappe
 def execute():
     properties_to_remove = frappe.get_all(
         "Property Setter",
-        {"doc_type": "Task", "property": ["in", ["permlevel", "default"]]},
+        filters={"doc_type": "Task"},
+        or_filters={
+            "property": ["in", ["permlevel", "default"]],
+            "field_name": "status",
+        },
         pluck="name",
-    ) + ["Task-status-options"]
+    )
     for p in properties_to_remove:
         frappe.delete_doc_if_exists("Property Setter", p, force=1)
 
