8bc6ecb4011647b2ab0a8408519d99cd228c728b
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2026-02-28 15:16:00 +0300

    convert leave balance to list

--- common/api/controllers/common/employee.py
+++ common/api/controllers/common/employee.py
@@ -5,6 +5,7 @@ from frappe.utils import getdate, cint
 from common.api.utils.resource import BaseResource
 from common.api.utils.decorators import safe_api
 
+
 class EmployeeResource(BaseResource):
     doctype = "Employee"
     fields = [
@@ -21,52 +22,79 @@ class EmployeeResource(BaseResource):
         @safe_api
         def _action(name):
             for_annual: int = frappe.request.values.get("for_annual", 1)
-            
+
             # Logic extracted from previous function
             if not name:
-                name = frappe.db.get_value("Employee", {"user_id": frappe.session.user}, "name")
+                name = frappe.db.get_value(
+                    "Employee", {"user_id": frappe.session.user}, "name"
+                )
 
             if not name:
-                frappe.throw(_("Employee ID (name) must be provided"), frappe.MandatoryError)
+                frappe.throw(
+                    _("Employee ID (name) must be provided"), frappe.MandatoryError
+                )
 
             if not frappe.db.exists("Employee", name):
-                frappe.throw(_("The requested employee could not be found"), frappe.DoesNotExistError)
-                
-            employee = frappe.get_doc("Employee", name)
-            if not frappe.has_permission("Employee", "read", employee, frappe.session.user, False):
-                raise frappe.PermissionError(_("You do not have permission to access employee details"))
+                frappe.throw(
+                    _("The requested employee could not be found"),
+                    frappe.DoesNotExistError,
+                )
 
-            from hrms.hr.doctype.leave_application.leave_application import get_leave_details
+            employee = frappe.get_doc("Employee", name)
+            if not frappe.has_permission(
+                "Employee", "read", employee, frappe.session.user, False
+            ):
+                raise frappe.PermissionError(
+                    _("You do not have permission to access employee details")
+                )
+
+            from hrms.hr.doctype.leave_application.leave_application import (
+                get_leave_details,
+            )
 
             date = getdate()
-            leave_map = {}
+            leave_map = []
             leave_details = get_leave_details(employee.name, date)
             allocation = leave_details["leave_allocation"]
 
             if cint(for_annual) == 1:
-                annual_leave = frappe.db.get_single_value("Company Policy", "annual_leave_type")
+                annual_leave = frappe.db.get_single_value(
+                    "Company Policy", "annual_leave_type"
+                )
                 if not annual_leave:
-                    frappe.throw(_("Data for Annual Leave Type is not available"), frappe.DoesNotExistError)
-                    
+                    frappe.throw(
+                        _("Data for Annual Leave Type is not available"),
+                        frappe.DoesNotExistError,
+                    )
+
                 details = allocation.get(annual_leave, {})
-                leave_map[annual_leave] = {
-                    "leave_type": annual_leave,
-                    "allocated_leaves": details.get("total_leaves", 0.0),
-                    "balance_leaves": details.get("remaining_leaves", 0.0),
-                    "expired_leaves": details.get("expired_leaves", 0.0),
-                    "leaves_pending_approval": details.get("leaves_pending_approval", 0.0),
-                }
+                leave_map.append(
+                    {
+                        "leave_type": annual_leave,
+                        "allocated_leaves": details.get("total_leaves", 0.0),
+                        "balance_leaves": details.get("remaining_leaves", 0.0),
+                        "expired_leaves": details.get("expired_leaves", 0.0),
+                        "leaves_pending_approval": details.get(
+                            "leaves_pending_approval", 0.0
+                        ),
+                    }
+                )
                 return leave_map, "Employee Annual Leave Balance Details"
 
             for leave_type, details in allocation.items():
-                leave_map[leave_type] = {
-                    "leave_type": leave_type,
-                    "allocated_leaves": details.get("total_leaves", 0.0),
-                    "balance_leaves": details.get("remaining_leaves", 0.0),
-                    "expired_leaves": details.get("expired_leaves", 0.0),
-                    "leaves_pending_approval": details.get("leaves_pending_approval", 0.0),
-                }
+                leave_map.append(
+                    {
+                        "leave_type": leave_type,
+                        "allocated_leaves": details.get("total_leaves", 0.0),
+                        "balance_leaves": details.get("remaining_leaves", 0.0),
+                        "expired_leaves": details.get("expired_leaves", 0.0),
+                        "leaves_pending_approval": details.get(
+                            "leaves_pending_approval", 0.0
+                        ),
+                    }
+                )
             return leave_map, "Employee Leave Balance Details"
+
         return _action
 
     @classmethod
@@ -74,9 +102,13 @@ class EmployeeResource(BaseResource):
         routes = super().get_routes()
         name = cls.resource_name or cls.doctype.lower().replace(" ", "-")
         base_url = f"{cls.url_prefix}/{name}"
-        
+
         # Add custom route
         routes.append(
-            Rule(f"{base_url}/<path:name>/leave-balance", methods=["GET"], endpoint=cls.leave_balance_action())
+            Rule(
+                f"{base_url}/<path:name>/leave-balance",
+                methods=["GET"],
+                endpoint=cls.leave_balance_action(),
+            )
         )
         return routes
