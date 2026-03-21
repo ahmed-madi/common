14222ecdf8038790295864b436430e604d865a1d
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2026-03-21 15:36:32 +0300

    unifiy hr support requests

--- common/api/routes/hr_requests/__init__.py
+++ common/api/routes/hr_requests/__init__.py
@@ -1,5 +1,14 @@
-from common.api.controllers.hr_requests.unified import UnifiedRequestResource, UnifiedRequestStatusResource
+from common.api.controllers.hr_requests.unified import (
+    UnifiedRequestResource,
+    UnifiedRequestStatusResource,
+)
+from common.api.controllers.hr_requests.unified_hr_support import (
+    HRSupportResource,
+    HRSupportStatusResource,
+)
 
 employee_requests_rules = []
 employee_requests_rules += UnifiedRequestResource.get_routes()
 employee_requests_rules += UnifiedRequestStatusResource.get_routes()
+employee_requests_rules += HRSupportResource.get_routes()
+employee_requests_rules += HRSupportStatusResource.get_routes()
